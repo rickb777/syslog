a064326cf3be3678d569347a975fff19142d1c64
Rick <1358735+rickb777@users.noreply.github.com>
2025-07-30 15:51:06 +0100

    documentation

--- example_server/send-test-messages.sh
+++ example_server/send-test-messages.sh
@@ -1,13 +1,23 @@
 #!/bin/bash -ex
-PORT=${1:-1514}
-# The example server should be started already, listening on UDP $PORT.
-# For example, use
-#   go build -o syslog ./example_server && ./syslog -port 1514 -file output.log
 
-logger --server localhost --port $PORT -t taggy -s    --rfc3164 --priority user.notice Message RFC3164 one
-logger --server localhost --port $PORT -t taggy -s -i --rfc3164 --priority user.notice Message RFC3164 two
-logger --server localhost --port $PORT -t taggy -s -i --rfc5424 --priority user.warning Message RFC5424 three
-logger --server localhost --port $PORT -t taggy -s -i --rfc5424=notq --priority user.err Message RFC5424 four
-logger --server localhost --port $PORT -t taggy -s -i --rfc5424=notime --priority user.info Message RFC5424 five
-logger --server localhost --port $PORT -t taggy -s -i --rfc5424=nohost --priority user.info Message RFC5424 six
-logger --server localhost --port $PORT -t taggy -s    --rfc5424 --priority user.debug Message RFC5424 seven
+HOST=${1:-localhost}
+PORT=${2:-1514}
+
+# Before running this script, the example server should be running already on $HOST, listening on UDP $PORT.
+#
+# Try this out using
+#   go build -o syslog ./example_server && ./syslog -port 1514 -file output.log
+#
+# For cross-compilation, the usual environment variables work fine, e.g.
+#   GOOS=linux GOARCH=arm64 go build -o syslog.arm64 ./example_server
+
+OPTS="--server $HOST --port $PORT -t taggy --stderr"
+# the obsolete RFC3164 message format (still widely used)
+logger $OPTS    --rfc3164        --priority user.notice  Message RFC3164 one
+logger $OPTS -i --rfc3164        --priority user.notice  Message RFC3164 two
+# the modern RFC5424 message format
+logger $OPTS -i --rfc5424        --priority user.warning Message RFC5424 three
+logger $OPTS -i --rfc5424=notq   --priority user.err     Message RFC5424 four
+logger $OPTS -i --rfc5424=notime --priority user.info    Message RFC5424 five
+logger $OPTS -i --rfc5424=nohost --priority user.info    Message RFC5424 six
+logger $OPTS    --rfc5424        --priority user.debug   Message RFC5424 seven
